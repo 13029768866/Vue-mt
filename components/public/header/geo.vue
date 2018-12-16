167ef5debb2e47dc6578fec89b313f84b85089af
13029768866 <13029768866@163.com>
2018-12-16 21:19:13 +0800

    头部构建

---
+++ components/public/header/geo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link 
+      class="changeCity" 
+      to="/changeCity">切换城市</nuxt-link>[黑龙江 吉林 辽宁]
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+</style>
---
+++ components/public/header/searchbar.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="search-panel">
+    <el-row class="m-header-searchbar">
+      <el-col 
+        :span="3"
+        class="left">
+        <img 
+          src="http://s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png" 
+        >
+      </el-col>
+      <el-col 
+        :span="15"
+        class="center">
+        <div class="wrapper">
+          <el-input 
+            v-model="search"
+            placeholder="搜索商家或地点"
+            @focus="focus"
+            @blur="blur"/>
+          <button class="el-button el-button--primary">
+            <i class="el-icon-search"/>
+          </button>
+          <dl
+            v-if="isHotPlace" 
+            class="hotPlace">
+            <dt>热们搜索</dt>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+          <dl 
+            v-if="isSearchList"
+            class="searchList">
+            <dd>炒面</dd>
+            <dd>炒面</dd>
+            <dd>炒面</dd>
+            <dd>炒面</dd>
+            <dd>炒面</dd>
+          </dl>
+        </div>
+        <p class="suggest">
+          <a href="#">雪中悍刀行</a>
+          <a href="#">雪中悍刀行</a>
+          <a href="#">雪中悍刀行</a>
+          <a href="#">雪中悍刀行</a>
+          <a href="#">雪中悍刀行</a>
+        </p>
+        <ul class="nav">
+          <li>
+            <nuxt-link 
+              to="/"
+              class="takeout"
+            >剑来</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link 
+              to="/"
+              class="movie"
+            >剑来</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link 
+              to="/"
+              class="hotel"
+            >剑来</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link 
+              to="/"
+              class="apartment"
+            >剑来</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link 
+              to="/"
+              class="business"
+            >剑来</nuxt-link>
+          </li>
+        </ul>
+      </el-col>
+      <el-col 
+        :span="6"
+        class="right">
+        <ul class="security">
+          <li><i class="refund"/><p class="txt">随时退</p></li>
+          <li><i class="single"/><p class="txt">不满意免单</p></li>
+          <li><i class="overdue"/><p class="txt">过期退</p></li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      // 判断是否有内容
+      search:'',
+      // 判断是否聚焦
+      isFocus: false
+    }
+  } ,
+   computed:{
+      isHotPlace(){return this.isFocus && !this.search}       
+      ,
+      isSearchList(){return this.isFocus && this.search}        
+    } ,
+    methods:{
+      focus(){this.isFocus = true },          
+      blur(){
+        setTimeout(() => {
+          this.isFocus = false
+        }, 200);  
+      }
+    }
+};
+</script>
+<style lang="scss">
+</style>
